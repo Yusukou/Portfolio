a42f229df33d1e381b8bcfc7f0daef82e0fd0858
o <yusuf.yeni@epitech.eu>
2025-11-17 15:51:27 +0100

    cv

--- portfolio/components/cv-download.jsx
+++ portfolio/components/cv-download.jsx
@@ -3,7 +3,7 @@ export default function CvDownload() {
     <div className="bg-gray-100 py-8 md:py-10 text-center">
       <div className="container mx-auto px-4">
         <a
-          href="/assets/Cv Yusuf Yeni.pdf"
+          href="/assets/CV Yusuf_Yeni.pdf"
           download
           className="inline-block bg-[#0057b8] text-white py-2 px-4 md:py-3 md:px-6 rounded-lg hover:bg-blue-700 transition duration-300">
           Télécharger mon CV
